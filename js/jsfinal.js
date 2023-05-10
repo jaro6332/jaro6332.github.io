3a9c1eb4f0dd70a6f8f2b54159e462b5bd29f5d7
jaro6332 <80434006+jaro6332@users.noreply.github.com>
2023-05-09 18:23:54 -0600

    jsfinal

--- js/jsfinal.js
+++ js/jsfinal.js
@@ -7,49 +7,65 @@ const player2Score = document.getElementById("player2-score");
 const startButton = document.getElementById("start-button");
 const stopButton = document.getElementById("stop-button");
 
-let ballX = 245;
-let ballY = 145;
+let ballX = 500;
+let ballY = 300;
 let ballSpeedX = 5;
 let ballSpeedY = 5;
-let leftPaddleY = 115;
-let rightPaddleY = 115;
+let leftPaddleY = 250;
+let rightPaddleY = 250;
 let player1ScoreValue = 0;
 let player2ScoreValue = 0;
 let gameInterval;
-const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff'];
+const colors = ['#d24787', '#7f3667', '#e5a0c6', '#a53e76', '#ff1493'];
 const randomColor = colors[Math.floor(Math.random() * colors.length)];
 function moveBall() {
-  ballX += ballSpeedX;
-  ballY += ballSpeedY;
-
-  if (ballY > 290 || ballY < 0) {
-    ballSpeedY = -ballSpeedY;
-  }
-
-  if (ballX > 900) {
-    ballSpeedX = -ballSpeedX;
-    player1ScoreValue++;
-    player1Score.innerText = player1ScoreValue;
-    gameBoard.style.backgroundColor = randomColor;
-  }
-
-  if (ballX < 10) {
-    ballSpeedX = -ballSpeedX;
-    player2ScoreValue++;
-    player2Score.innerText = player2ScoreValue;
-    gameBoard.style.backgroundColor = randomColor;
-  }
-
-  if (ballX < 20 && ballY >= leftPaddleY && ballY <= leftPaddleY + 70) {
-    ballSpeedX = -ballSpeedX;
-  }
-
-  if (ballX > 470 && ballY >= rightPaddleY && ballY <= rightPaddleY + 70) {
-    ballSpeedX = -ballSpeedX;
-  }
-
-  ball.style.left = ballX + "px";
-  ball.style.top = ballY + "px";
+    ballX += ballSpeedX;
+  
+    // Move the ball vertically
+    ballY += ballSpeedY;
+    
+    // Check for collision with the left paddle
+    if (ballX <= 30 && ballY > leftPaddleY && ballY < leftPaddleY + 140) {
+      ballSpeedX *= -1;
+      ballSpeedY = (ballY - (leftPaddleY + 70)) * 0.1;
+    }
+    
+    // Check for collision with the right paddle
+    if (ballX >= 970 && ballY > rightPaddleY && ballY < rightPaddleY + 140) {
+      ballSpeedX *= -1;
+      ballSpeedY = (ballY - (rightPaddleY + 70)) * 0.1;
+    }
+    
+    // Check for collision with the top and bottom walls
+    if (ballY <= 10 || ballY >= 590) {
+      ballSpeedY *= -1;
+    }
+    
+    // Check for scoring on the left side
+    if (ballX <= 0) {
+      player2ScoreValue++;
+      player2Score.innerHTML = player2ScoreValue;
+      ballX = 500;
+      ballY = 300;
+      ballSpeedY = 5;
+      ballSpeedX = -5;
+      gameBoard.style.backgroundColor = randomColor;
+    }
+    
+    // Check for scoring on the right side
+    if (ballX >= 1000) {
+      player1ScoreValue++;
+      player1Score.innerHTML = player1ScoreValue;
+      ballX = 500;
+      ballY = 300;
+      ballSpeedY = 5;
+      ballSpeedX = 5;
+      gameBoard.style.backgroundColor = randomColor;
+    }
+    
+    // Move the ball to its new position
+    ball.style.left = ballX + "px";
+    ball.style.top = ballY + "px";
 }
 
 function moveLeftPaddle(event) {
@@ -57,8 +73,8 @@ function moveLeftPaddle(event) {
   if (leftPaddleY < 0) {
     leftPaddleY = 0;
   }
-  if (leftPaddleY > 230) {
-    leftPaddleY = 230;
+  if (leftPaddleY > 500) {
+    leftPaddleY = 500;
   }
   leftPaddle.style.top = leftPaddleY + "px";
 }
@@ -68,8 +84,8 @@ function moveRightPaddle(event) {
   if (rightPaddleY < 0) {
     rightPaddleY = 0;
   }
-  if (rightPaddleY > 230) {
-    rightPaddleY = 230;
+  if (rightPaddleY > 500) {
+    rightPaddleY = 500;
   }
   rightPaddle.style.top = rightPaddleY + "px";
 }
